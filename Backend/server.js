85826578b1a261ee45cede6879e586d8f09546ee
Divyang Chunara <divyangchunara@192.168.1.2>
2025-11-21 20:44:36 +0530

    Remove node_modules from repository

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 import router from "./Routes/route.js";
 
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());

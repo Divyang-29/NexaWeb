3b3fee5a1311ed91fb280e7f6edc0d9b02a7a7f9
Divyang Chunara <divyangchunara@192.168.1.3>
2025-11-25 16:47:55 +0530

    Updated backend and frontend

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(cors());
 
-app.use("/nexaweb", router);
+app.use("/", router);
 
 const connectDB = async () => {
   try {
